76f183cc324453fd03c12d53540c2c490b14306b
Shafeequemk80 <64883556+Shafeequemk80@users.noreply.github.com>
2024-05-19 16:41:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port:5000,
     proxy:{
       '/api':{
-        target:'https://ums-react.onrender.com/',
+        target:'http://localhost:8000/',
         changeOrigin:true
       }
     }
