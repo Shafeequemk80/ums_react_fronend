ff5355227379df5cba60fcfb6cea479cfc5d0ac3
Shafeequemk80 <shafeequemk80@gmail.com>
2024-08-09 10:24:08 +0530

    hsu

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://ums-react-backend-code.onrender.com/";
-//export const BASE_URL = "http://localhost:8000/";
+//export const BASE_URL = "https://ums-react-backend-code.onrender.com/";
+export const BASE_URL = "http://localhost:8000/";
--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,10 @@ export default defineConfig({
     proxy:{
       '/api':{
 
-        target: 'https://ums-react-backend-code.onrender.com/', // Added https:// to the target URL
-       //  target: 'http://localhost:8000', // For local development
+       // target: 'https://ums-react-backend-code.onrender.com', // Added https:// to the target URL
+        target: 'http://localhost:8000', // For local development
         changeOrigin: true,
-        target:'http://localhost:8000/',
-        changeOrigin:true
+        
       }
     }
   }
