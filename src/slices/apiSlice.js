58f14ace087b4e8b3586e349fe6a629138f9eb9d
Shafeequemk80 <shafeequemk80@gmail.com>
2024-08-13 14:04:30 +0530

    added env file used

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL
 
-const baseQuery = fetchBaseQuery({ baseUrl: "/api/users" });
+const baseQuery = fetchBaseQuery({ baseUrl: `${BASE_URL}/api/users` });
 
 export const apiSlice = createApi({
   baseQuery,
